9c255847894063667578010b1d3b5b99c5a13674
Victor Brum <83709796+brumvictor@users.noreply.github.com>
2024-12-11 11:59:09 -0300

    add UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Consultar CEP</h1>
+      <div className="input-group">
+        <input type="text" placeholder="Digite o CEP (8 dígitos)" maxLength={9} />
+        <button>Buscar</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
